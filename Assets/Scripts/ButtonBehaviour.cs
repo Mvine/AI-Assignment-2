d2b76d2f49920447b17a146b7ae62b9205c49467
Marcus Vine <marcus.vine@uoit.net>
2022-03-09 13:57:56 -0500

    basic scripts

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    public enum State { Empty, X, O };
+
+    [SerializeField]
+    private GameManager gameManager;
+
+    public State state = State.Empty;
+
+    private TextMeshProUGUI tmp;
+
+    void Start()
+    {
+        tmp = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+
+    public void SetText()
+    {
+        if (state != State.Empty) { return; }
+
+        if (gameManager.turn == GameManager.Turn.Player)
+        {
+            tmp.text = "X";
+        }
+
+        if (gameManager.turn == GameManager.Turn.AI)
+        {
+            tmp.text = "O";
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //things that need to be managed by the game
+    
+    //whose turn it is
+    //what to do when someone wins
+    //restarting the game
+
+
+    public enum Turn {Player, AI};
+
+    public Turn turn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
